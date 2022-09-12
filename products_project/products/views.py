5748c55fd9f0a1723dc9d86e6d072e1878e50b8e
cTaylorDFF <ctaylor@dffcorp.com>
2022-09-12 07:26:07 -0400

    Created Get_All_Products Request + Created Add_Product Request

--- products_project/products/views.py
+++ products_project/products/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status       #Utilized for creating more readable code
+from .serializers import ProductSerializer
+from .models import Product
+from products import serializers
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def products_list(request):
+
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
